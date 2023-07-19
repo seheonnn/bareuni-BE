7ee09af3183ad3d91180b9b3b9d330180d122042
seheonnn <ho78901@naver.com>
2023-07-19 16:27:04 +0900

    Feat: Community 글 작성

--- src/main/java/com/umc/BareuniBE/controller/CommunityController.java
+++ src/main/java/com/umc/BareuniBE/controller/CommunityController.java
@@ -1,5 +1,10 @@
 package com.umc.BareuniBE.controller;
 
+import com.umc.BareuniBE.dto.CommunityReq;
+import com.umc.BareuniBE.dto.CommunityRes;
+import com.umc.BareuniBE.global.BaseException;
+import com.umc.BareuniBE.global.BaseResponse;
+import com.umc.BareuniBE.service.CommunityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,4 +13,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/community")
 public class CommunityController {
 
+    private final CommunityService communityService;
+
+    @PostMapping("")
+    public BaseResponse<CommunityRes.CommunityCreateRes> createCommunity(@RequestBody CommunityReq.CommunityCreateReq request) throws BaseException {
+        return new BaseResponse<>(communityService.createCommunity(request));
+    }
 }
--- src/main/java/com/umc/BareuniBE/service/CommunityService.java
+++ src/main/java/com/umc/BareuniBE/service/CommunityService.java
@@ -1,10 +1,34 @@
 package com.umc.BareuniBE.service;
 
+import com.umc.BareuniBE.dto.CommunityReq;
+import com.umc.BareuniBE.dto.CommunityRes;
+import com.umc.BareuniBE.entities.Community;
+import com.umc.BareuniBE.entities.User;
+import com.umc.BareuniBE.global.BaseException;
+import com.umc.BareuniBE.repository.CommunityRepository;
+import com.umc.BareuniBE.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.umc.BareuniBE.global.BaseResponseStatus.USERS_EMPTY_USER_ID;
 
 @Service
 @RequiredArgsConstructor
 public class CommunityService {
+
+    private final UserRepository userRepository;
+    private final CommunityRepository communityRepository;
+
+    public CommunityRes.CommunityCreateRes createCommunity(CommunityReq.CommunityCreateReq request) throws BaseException {
+        User user = userRepository.findById(request.getUserIdx())
+                .orElseThrow(() ->  new BaseException(USERS_EMPTY_USER_ID));
+
+        Community newCommunity = Community.builder()
+                .user(user)
+                .content(request.getContent())
+                .build();
+        Community community = communityRepository.saveAndFlush(newCommunity);
+        return new CommunityRes.CommunityCreateRes(community);
+    }
 
 }
