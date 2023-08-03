2fa7f1cee0b2b26423b5e4b4a2d954a52ddc92b5
seheonnn <ho78901@naver.com>
2023-08-03 19:01:08 +0900

    Docs: Swagger 설정

--- src/main/java/com/umc/BareuniBE/controller/CommunityController.java
+++ src/main/java/com/umc/BareuniBE/controller/CommunityController.java
@@ -9,7 +9,6 @@ import com.umc.BareuniBE.service.CommunityService;
 import lombok.RequiredArgsConstructor;
 
 import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -17,7 +16,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +27,14 @@ public class CommunityController {
     private final CommunityService communityService;
 
     // 커뮤니티 글 작성
+    @ApiOperation(value = "커뮤니티 글 작성", notes = "ex) http://localhost:8080/community\n\n" +
+            "{\n" +
+            "\n" +
+            "\"userIdx\":1,\n" +
+            "\n" +
+            "\"content\":\"커뮤니티 글입니다.\"\n" +
+            "\n" +
+            "}")
     @PostMapping("")
     public BaseResponse<CommunityRes.CommunityCreateRes> createCommunity(@RequestBody CommunityReq.CommunityCreateReq request) throws BaseException {
         return new BaseResponse<>(communityService.createCommunity(request));
@@ -45,30 +51,49 @@ public class CommunityController {
 
 
     // 커뮤니티 글 상세 조회
+    @ApiOperation(value = "커뮤니티 글 상세 조회", notes = "ex) http://localhost:8080/community/1")
     @GetMapping("/{communityIdx}")
     public BaseResponse<CommunityRes.CommunityDetailRes> getCommunityDetails( @PathVariable Long communityIdx ) throws BaseException {
         return new BaseResponse<>(communityService.getCommunityDetails(communityIdx));
     }
 
     // 커뮤니티 글 수정
+    @ApiOperation(value = "커뮤니티 글 수정", notes = "ex) http://localhost:8080/community/1\n\n" +
+            "{\n\n" +
+            "\"userIdx\":1,\n\n" +
+            "\"content\":\"커뮤니티 글 수정입니다.\"\n\n" +
+            "}")
     @PatchMapping("/{communityIdx}")
     public BaseResponse<CommunityRes.CommunityCreateRes> updateCommunity(@PathVariable Long communityIdx, @RequestBody CommunityReq.CommunityCreateReq request) throws BaseException {
         return new BaseResponse<>(communityService.updateCommunity(communityIdx, request));
     }
 
     // 커뮤니티 글 삭제
+    @ApiOperation(value = "커뮤니티 글 삭제", notes = "ex) http://localhost:8080/community/1\n\n" +
+            "{\n\n" +
+            "\"userIdx\":1\n\n" +
+            "}")
     @DeleteMapping("/{communityIdx}")
     public BaseResponse<String> deleteCommunity(@PathVariable Long communityIdx, @RequestBody Map<String, Long> requestBody) throws BaseException {
         return new BaseResponse<>(communityService.deleteCommunity(communityIdx, requestBody.get("userIdx")));
     }
 
-    // 커뮤니티 좋아요
+    // 커뮤니티 글 좋아요
+    @ApiOperation(value = "커뮤니티 글 좋아요", notes = "ex) http://localhost:8080/community/1/like\n\n" +
+            "{\n\n" +
+            "\"userIdx\":1\n\n" +
+            "}")
     @PostMapping("/{communityIdx}/like")
     public BaseResponse<String> likeToggle(@PathVariable Long communityIdx, @RequestBody Map<String, Long> requestBody) throws BaseException {
         return new BaseResponse<>(communityService.likeToggle(requestBody.get("userIdx"), communityIdx));
     }
 
     // 커뮤니티 댓글 작성
+    @ApiOperation(value = "커뮤니티 댓글 작성", notes = "ex) http://localhost:8080/community/1/comment\n\n" +
+            "{\n\n" +
+            "\"userIdx\":1,\n\n" +
+            "\"comment\":\"커뮤니티 댓글입니다.\"\n\n" +
+            "}")
     @PostMapping("/{communityIdx}/comment")
     public BaseResponse<CommunityRes.CommentCreateRes> createComment(@PathVariable Long communityIdx, @RequestBody CommunityReq.CommentCreateReq request) throws BaseException {
         return new BaseResponse<>(communityService.createComment(communityIdx, request));
--- src/main/java/com/umc/BareuniBE/controller/UserController.java
+++ src/main/java/com/umc/BareuniBE/controller/UserController.java
@@ -6,13 +6,11 @@ import com.umc.BareuniBE.global.BaseResponse;
 import com.umc.BareuniBE.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
