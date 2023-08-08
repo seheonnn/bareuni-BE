8fdd917ef53f71fe6d6008252f5f88c245be06ff
khw18 <gmsb14@naver.com>
2023-08-09 01:12:35 +0900

    Feat: 이메일 인증 & 비밀번호 찾기

--- src/main/java/com/umc/BareuniBE/global/BaseResponseStatus.java
+++ src/main/java/com/umc/BareuniBE/global/BaseResponseStatus.java
@@ -30,6 +30,8 @@ public enum BaseResponseStatus {
     POST_USERS_INVALID_EMAIL(false, 2016, "이메일 형식을 확인해주세요."),
     POST_USERS_EXISTS_EMAIL(false,2017,"중복된 이메일입니다."),
 
+    POST_USERS_NOT_FOUND_EMAIL(false,2018,"존재하지 않는 이메일입니다."),
+
 
 
     /**
---
+++ src/main/java/com/umc/BareuniBE/service/EmailService.java
@@ -0,0 +1,5 @@
+package com.umc.BareuniBE.service;
+
+public interface EmailService {
+    String sendSimpleMessage(String to)throws Exception;
+}
