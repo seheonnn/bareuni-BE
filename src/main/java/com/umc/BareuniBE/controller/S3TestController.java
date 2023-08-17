4068a6d7d201ac42fe86f47ab391cfcb62edb7f2
seheonnn <ho78901@naver.com>
2023-08-17 18:48:06 +0900

    [FIX] S3 배포 오류

--- src/main/java/com/umc/BareuniBE/controller/S3TestController.java
+++ src/main/java/com/umc/BareuniBE/controller/S3TestController.java
@@ -1,26 +1,26 @@
-//package com.umc.BareuniBE.controller;
-//
-//import com.umc.BareuniBE.service.UploadService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//@RequiredArgsConstructor
-//@RestController
-//@RequestMapping("/uploadImage")
-//public class S3TestController {
-//    private final UploadService uploadService;
-//    @PostMapping(value = "")
-//    public ResponseEntity uploadImage(@RequestPart(value = "file", required = false) MultipartFile file) throws Exception{
-//        String url = uploadService.uploadImage(file);
-//        return new ResponseEntity<>(url, HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping(value = "")
-//    public ResponseEntity deleteImage(@RequestPart(value = "path", required = false) String path) {
-//        String image = path.substring(path.lastIndexOf('/')+1);
-//        return new ResponseEntity<>(uploadService.deleteImage(image), HttpStatus.OK);
-//    }
-//}
+package com.umc.BareuniBE.controller;
+
+import com.umc.BareuniBE.service.UploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/uploadImage")
+public class S3TestController {
+    private final UploadService uploadService;
+    @PostMapping(value = "")
+    public ResponseEntity uploadImage(@RequestPart(value = "file", required = false) MultipartFile file) throws Exception{
+        String url = uploadService.uploadImage(file);
+        return new ResponseEntity<>(url, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "")
+    public ResponseEntity deleteImage(@RequestPart(value = "path", required = false) String path) {
+        String image = path.substring(path.lastIndexOf('/')+1);
+        return new ResponseEntity<>(uploadService.deleteImage(image), HttpStatus.OK);
+    }
+}
