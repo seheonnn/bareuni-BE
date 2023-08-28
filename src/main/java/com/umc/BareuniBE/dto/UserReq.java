77b697ac3043148afda1dc408f3dd8d18931c9da
seheonnn <ho78901@naver.com>
2023-08-28 14:27:42 +0900

    [REFACTOR] 회원가입 ModelAttribute 적용 및 Response 수정

--- src/main/java/com/umc/BareuniBE/dto/UserReq.java
+++ src/main/java/com/umc/BareuniBE/dto/UserReq.java
@@ -1,17 +1,36 @@
 package com.umc.BareuniBE.dto;
 
 import com.umc.BareuniBE.entities.User;
+import com.umc.BareuniBE.global.enums.GenderType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 public class UserReq {
 
     @Getter
     @Setter
-    public static class UserJoinReq extends User { // 회원가입 요청
-       //private User user;
+    public static class UserJoinRequestWrapper { // 회원가입 요청
+        private MultipartFile file;
+        private UserJoinReq userJoinReq;
+
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class UserJoinReq {
+        private String email;
+        private Long age;
+        private String password;
+        private String nickname;
+
+        private GenderType gender;
+        private String provider;
+        private boolean ortho;
     }
 
     @NoArgsConstructor
